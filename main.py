e884516ce584ee23660813182df693afd03d91d4
Tanner Blair <tannerblair@outlook.com>
2021-06-02 12:11:31 -0500

    cleaned up functions, and it now processes all tables in the db

--- main.py
+++ main.py
@@ -3,43 +3,74 @@ import sqlite3 as db
 from pathlib import Path
 
 
-def create_table_from_layout_file(conn: db.Connection, prop_layout_file: Path):
-    with prop_layout_file.open(encoding="utf-8-sig") as file:
+
+def create_db_table(conn: db.Connection, layout_file: Path):
+    print(f"Creating table for {layout_file.stem.lower()}")
+    with layout_file.open(encoding="utf-8-sig") as file:
         reader = csv.reader(file)
         cols = []
         for row in csv.reader(file):
             if row[0] != 'filler':
-                cols.append(f"{row[0]} {row[1]}")
-        conn.execute(f"""CREATE TABLE IF NOT EXISTS {prop_layout_file.stem} ({', '.join(cols)});""")
+                key = row[0].replace('/ ', '_').replace(' ', '_')
+                cols.append(f"{key} {row[1]}")
+        exec_string = f"""CREATE TABLE IF NOT EXISTS {layout_file.stem.lower()} ({', '.join(cols)});"""
+        try:
+            conn.execute(exec_string)
+        except db.OperationalError as e:
+            print(exec_string)
+            raise db.OperationalError(e)
 
 
-if __name__ == '__main__':
-    conn = db.connect('tcad2021.db')
-    prop_layout_file = Path("layout-files/2020/PROP.csv")
-    prop_file = Path("source-files/2020/PROP.TXT")
-    create_table_from_layout_file(conn, prop_layout_file)
+def get_field_info(layout_file: Path):
     fields = {}
-    with prop_layout_file.open(encoding="utf-8-sig") as layout_file:
-        reader = csv.reader(layout_file)
-        for row in csv.reader(layout_file):
+    with layout_file.open(encoding="utf-8-sig") as file:
+        reader = csv.reader(file)
+        for row in csv.reader(file):
             if row[0] != 'filler':
-                fields[row[0]] = {}
-                fields[row[0]]['start'] = int(row[2])-1
-                fields[row[0]]['end'] = int(row[3])
+                key = row[0].replace('/ ', '_').replace(' ', '_')
+                fields[key] = {}
+                try:
+                    fields[key]['start'] = int(row[2]) - 1
+                    fields[key]['end'] = int(row[3])
+                except ValueError as e:
+                    print(fields)
+                    raise ValueError(e)
 
-    with prop_file.open() as file:
+    return fields
+
+
+def add_rows_to_db(conn: db.Connection, layout_file: Path, data_file: Path):
+    print(f"Adding rows for {layout_file}")
+    fields = get_field_info(layout_file)
+    with data_file.open() as file:
         lines = file.readlines()
-        entries = []
-        idx = 0
         for line in lines:
             entry = {}
             for name, value in fields.items():
                 val_string = line[value['start']:value['end']].strip()
                 val_string = val_string.replace('"', "'")
                 entry[name] = f'"{val_string}"'
-            exec_string = f"INSERT INTO PROP ({', '.join(entry.keys())}) VALUES ({', '.join(entry.values())})"
-            conn.execute(exec_string)
-            idx += 1
-            if not idx % 1000:
-                print(idx)
-    conn.commit()
+            exec_string = f"INSERT INTO {layout_file.stem.lower()} ({', '.join(entry.keys())}) VALUES ({', '.join(entry.values())})"
+            try:
+                conn.execute(exec_string)
+            except db.OperationalError as e:
+                print(exec_string)
+                raise db.OperationalError(e)
+
+
+def add_table(conn: db.Connection, layout_file: Path, data_file: Path):
+    print(f"Adding rows for {layout_file}")
+    create_db_table(conn, layout_file)
+    add_rows_to_db(conn, layout_file, data_file)
+
+
+if __name__ == '__main__':
+    print("Starting...")
+    src_file_dir = Path("source-files/2020")
+    layout_file_dir = Path("layout-files/2020")
+    db2020 = db.connect('tcad-2020.db')
+    for file in src_file_dir.iterdir():
+        print(f"Processing {file}")
+        layout = layout_file_dir / f"{file.stem}.CSV"
+        add_table(db2020, layout, file)
+    db2020.commit()
